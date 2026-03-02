9346ca7bda05eed4b39030635551eb93e151ea21
Shiffy Anny <shiffex12anny@gmail.com>
2026-03-02 14:17:26 +0100

    commit

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -12,6 +12,7 @@ import "swiper/css";
 import { MdOutlineFileUpload, MdVerified, MdCampaign } from "react-icons/md";
 import { FaStar } from "react-icons/fa";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 
 const LandingPage = () => {
@@ -373,47 +374,7 @@ const LandingPage = () => {
         </section>
 
         {/* ================= FOOTER ================= */}
-        <footer className="bg-gray-900 text-gray-400 py-10 px-6 md:px-16">
-          <div className="max-w-6xl mx-auto grid grid-cols-2 items-center justify-center md:items-start md:grid-cols-4 gap-8">
-            <div>
-              <h3 className="text-white font-semibold mb-3">U-Tune</h3>
-              <p className="text-sm">
-                The future of personalized caller branding.
-              </p>
-            </div>
-
-            <div>
-              <h4 className="text-white font-semibold mb-3">Features</h4>
-              <ul className="space-y-2 text-sm">
-                <li>Upload RBT</li>
-                <li>Verification</li>
-                <li>Advertising</li>
-              </ul>
-            </div>
-
-            <div>
-              <h4 className="text-white font-semibold mb-3">Company</h4>
-              <ul className="space-y-2 text-sm">
-                <li> <a href="/about" className="text-white hover:text-blue-300 mx-2">About</a> </li>
-                <li>Careers</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-
-            <div>
-              <h4 className="text-white font-semibold mb-3">Support</h4>
-              <ul className="space-y-2 text-sm">
-                <li> <a href="">Help Center</a> </li>
-                <li> <a href="/privacy" className="text-white hover:text-blue-300 mx-2">Privacy Policy</a> </li>
-                <li> <a href="/terms" className="text-white hover:text-blue-300 mx-2">Terms of Services</a> </li>
-              </ul>
-            </div>
-          </div>
-
-          <p className="text-center text-xs mt-8">
-            © 2026 U-Tune. All rights reserved.
-          </p>
-        </footer>
+        <Footer />
 
       </div>
       </div>
--- src/Review.jsx
+++ src/Review.jsx
@@ -2,6 +2,7 @@ import { MdOutlineFileUpload, MdVerified, MdCampaign, MdMic, MdBolt, MdPhone, Md
 import { FaApple, FaGooglePlay } from "react-icons/fa";
 import platstore from "./assets/playstore.png";
 import { FaStar } from "react-icons/fa";
+import Footer from "./Footer";
 
 
 const Review = () => {
@@ -152,47 +153,7 @@ const Review = () => {
               </section>
       
               {/* ================= FOOTER ===== ============ */}
-              <footer className="bg-gray-900 text-gray-400 py-10 px-6 md:px-16">
-                <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 items-center justify-center md:items-start gap-8">
-                  <div>
-                    <h3 className="text-white font-semibold mb-3">U-Tune</h3>
-                    <p className="text-sm">
-                      The future of personalized caller branding.
-                    </p>
-                  </div>
-      
-                  <div>
-                    <h4 className="text-white font-semibold mb-3">Features</h4>
-                    <ul className="space-y-2 text-sm">
-                      <li>Upload RBT</li>
-                      <li>Verification</li>
-                      <li>Advertising</li>
-                    </ul>
-                  </div>
-      
-                  <div>
-              <h4 className="text-white font-semibold mb-3">Company</h4>
-              <ul className="space-y-2 text-sm">
-                <li> <a href="/about" className="text-white hover:text-blue-300 mx-2">About</a> </li>
-                <li>Careers</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-
-            <div>
-              <h4 className="text-white font-semibold mb-3">Support</h4>
-              <ul className="space-y-2 text-sm">
-                <li> <a href="">Help Center</a> </li>
-                <li> <a href="/privacy" className="text-white hover:text-blue-300 mx-2">Privacy Policy</a> </li>
-                <li> <a href="/terms" className="text-white hover:text-blue-300 mx-2">Terms of Services</a> </li>
-              </ul>
-            </div>
-          </div>
-      
-                <p className="text-center text-xs mt-8">
-                  © 2026 U-Tune. All rights reserved.
-                </p>
-              </footer>
+              <Footer/>
 
     </div>
   );
